a115f81f9253ab4c4ea79854fbcf4d722fb08fbb
nrossini <45461285+nrossini@users.noreply.github.com>
2018-11-29 16:04:01 -0500

    Add files via upload
    Cy Young Exercise - from Kyle

---
+++ Cy Young Exercise.R
@@ -0,0 +1,48 @@
+##### Your goal: Come up with two models. The first model should predict how many wins
+##### a pitcher "deserves" based on his production. The second model should take into account 
+##### his overall team's production
+##### This is in response to DeGrom recently winning the CY Young with 
+
+#### install.packages("Lahman")
+require(Lahman)
+rm(list = ls())
+
+master <- Master
+pnames <- select(master, playerID, nameFirst, nameLast)
+
+pitch <- Pitching
+pitch <- merge(pitch, pnames)
+
+##### For Model 1
+
+##### Use as model set
+modernPitch <- filter(pitch, yearID>=2006 & yearID<=2015)
+modernPitch <- filter(modernPitch, modernPitch$IPouts >= 150)
+modernPitch <- filter(modernPitch, modernPitch$GS >= 5)
+
+#### Make new columns
+modernPitch <- mutate(modernPitch, K9=((SO*9)/(IPouts/3)))
+modernPitch <- mutate(modernPitch, BB9=((BB*9)/(IPouts/3)))
+modernPitch <- mutate(modernPitch, H9=(H*9)/(IPouts/3))
+
+#### Subset dataset by year, innings pitched, starts
+pitch16 <- filter(pitch, yearID==2016)
+pitch16 <- filter(pitch16, pitch16$IPouts >= 150)
+pitch16 <- filter(pitch16, pitch16$GS >= 5)
+
+#### Make new columns per nines
+pitch16 <- mutate(pitch16, K9=((SO*9)/(IPouts/3)))
+pitch16 <- mutate(pitch16, BB9=((BB*9)/(IPouts/3)))
+pitch16 <- mutate(pitch16, H9=(H*9)/(IPouts/3))
+
+
+#### For model 2
+
+teams <- Teams
+modernTeams <- filter(teams, yearID>=2006 & yearID<=2015)
+teams16 <- filter(teams, yearID==2016)
+####Select statement to determine what columns are of interest from Team table
+
+
+##### CY Young Finalists 2016
+##### cyyoung <- filter(pitch16, playerID== "porceri01" | playerID== "scherma01" | playerID== "lestejo01" | playerID== "hendrky01" | playerID== "verlaju01" | playerID== "klubeco01")
