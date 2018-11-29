6c45a1b06d75ff707ea7418647e742b25a795ef7
nrossini <45461285+nrossini@users.noreply.github.com>
2018-11-29 17:15:07 -0500

    Add files via upload
    Updated file after meeting Nov. 29

--- Cy Young Exercise.R
+++ Cy Young Exercise.R
@@ -5,6 +5,7 @@
 
 #### install.packages("Lahman")
 require(Lahman)
+require(dplyr)
 rm(list = ls())
 
 master <- Master
@@ -24,6 +25,9 @@ modernPitch <- filter(modernPitch, modernPitch$GS >= 5)
 modernPitch <- mutate(modernPitch, K9=((SO*9)/(IPouts/3)))
 modernPitch <- mutate(modernPitch, BB9=((BB*9)/(IPouts/3)))
 modernPitch <- mutate(modernPitch, H9=(H*9)/(IPouts/3))
+modernPitch <- mutate(modernPitch, WHIP = (BB + IBB + H) / (IPouts/3))
+modernPitch <- mutate(modernPitch, FIPnc = ((13*HR)+(3*(BB+HBP))-(2*SO))/(IPouts/3))
+modernPitch <- mutate(modernPitch, IP = (IPouts / 3))
 
 #### Subset dataset by year, innings pitched, starts
 pitch16 <- filter(pitch, yearID==2016)
@@ -34,6 +38,27 @@ pitch16 <- filter(pitch16, pitch16$GS >= 5)
 pitch16 <- mutate(pitch16, K9=((SO*9)/(IPouts/3)))
 pitch16 <- mutate(pitch16, BB9=((BB*9)/(IPouts/3)))
 pitch16 <- mutate(pitch16, H9=(H*9)/(IPouts/3))
+pitch16 <- mutate(pitch16, WHIP = (BB + IBB + H) / (IPouts/3))
+pitch16 <- mutate(pitch16, FIPnc = ((13*HR)+(3*(BB+HBP))-(2*SO))/(IPouts/3))
+pitch16 <- mutate(pitch16, IP = (IPouts / 3))
+pitch16 <- mutate(pitch16, Wdiff = W - expw)
+
+
+## pitch16 <- mutate(pitch16, 
+
+## FIP = ((13*HR)+(3*(BB+HBP))-(2*K))/IP 
+
+lm1 <- lm(W ~ K9 + BB9 + H9 + WHIP + ERA + FIPnc, data = modernPitch)
+summary(lm1)
+
+lm2 <- lm(W ~ K9 + BB9 + H9 + WHIP + ERA + FIPnc + IP, data = modernPitch)
+summary(lm2)
+
+lm3 <- lm(W ~ BB9 + H9 + WHIP + ERA + IP, data = modernPitch)
+summary(lm3)
+
+pitch16$expw <- predict(lm3, newdata = pitch16)
+
 
 
 #### For model 2
@@ -44,5 +69,7 @@ teams16 <- filter(teams, yearID==2016)
 ####Select statement to determine what columns are of interest from Team table
 
 
+
+
 ##### CY Young Finalists 2016
-##### cyyoung <- filter(pitch16, playerID== "porceri01" | playerID== "scherma01" | playerID== "lestejo01" | playerID== "hendrky01" | playerID== "verlaju01" | playerID== "klubeco01")
+cyyoung <- filter(pitch16, playerID== "porceri01" | playerID== "scherma01" | playerID== "lestejo01" | playerID== "hendrky01" | playerID== "verlaju01" | playerID== "klubeco01")
